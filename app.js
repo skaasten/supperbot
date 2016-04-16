a431f39ab97ef14c47d4880b91a5233988c37b1e
Shaun Kaasten <Shaun Kaasten>
2016-04-16 19:17:43 -0400

    what's for dinner

--- app.js
+++ app.js
@@ -2,7 +2,19 @@ var builder = require('botbuilder');
 var supperbot = new builder.TextBot();
 
 supperbot.add('/', function (session) {
-    session.send('Hello World');
+    if (!session.userData.menu) {
+      session.beginDialog('/menu');
+    } else {
+      session.send("Hi, tonight you're having %s", session.userData.menu);
+    }
 });
-
+supperbot.add('/menu', [
+  function (session) {
+    builder.Prompts.text(session, "What's for dinner tonight?");
+  },
+  function (session, result) {
+    session.userData.menu = result.response;
+    session.endDialog();
+  }
+]);
 supperbot.listenStdin();
