8f22d08a70d90444469e4b8a4f3675f70e7cb7f8
Shaun Kaasten <Shaun Kaasten>
2016-04-16 19:52:14 -0400

    switch to botconnectorbot

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var builder = require('botbuilder');
-var supperbot = new builder.TextBot();
+var supperbot = new builder.BotConnectorBot({ appId: 'YourAppId', appSecret: 'YourAppSecret' });
 
 supperbot.add('/', function (session) {
     if (!session.userData.menu) {
@@ -17,4 +17,10 @@ supperbot.add('/menu', [
     session.endDialog();
   }
 ]);
-supperbot.listenStdin();
+
+var restify = require('restify');
+var server = restify.createServer();
+server.post('/v1/messages', supperbot.verifyBotFramework(), supperbot.listen());
+server.listen(8080, function () {
+    console.log('%s listening to %s', server.name, server.url); 
+});
