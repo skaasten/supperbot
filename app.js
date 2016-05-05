76bb07e098dd9f4ab3c51ed55c495e3337657c0f
Shaun Kaasten <shaunk@gmail.com>
2016-05-05 01:11:42 -0400

    Start of dinners for the week

--- app.js
+++ app.js
@@ -2,43 +2,18 @@ var builder = require('botbuilder');
 var supperbot = new builder.BotConnectorBot({ appId: process.env.APP_ID, appSecret: process.env.APP_SECRET });
 var moment = require('moment');
 
-supperbot.add('/', function (session) {
-    if (!session.userData.schedule) {
-      session.beginDialog('/menu');
-    } else {
-      if ((session.message.text.indexOf('no') !== -1) ||
-          (session.message.text.indexOf('No') !== -1)) {
-        session.userData.menu = null;
-        session.send("Okay, we can have something else - what do you want?");
-        session.beginDialog('/menu');
-      } else {
-        session.send("Hi, tonight you're having %s.\n" +
-                     "We need to %s %s from now.",
-                     session.userData.menu,
-                     session.userData.preparation,
-                     moment(session.userData.schedule).fromNow()
-                    );
-
-      }
-    }
-});
-supperbot.add('/menu', [
+var weeklyDinner = {
+  'Monday': undefined,
+  'Tuesday': undefined,
+  'Wednesday': undefined,
+  'Thursday': undefined,
+  'Friday': undefined
+};
+supperbot.add('/', [
   function (session) {
-    builder.Prompts.text(session, "What's for dinner tonight?");
-  },
-  function (session, result) {
-    session.userData.menu = result.response;
-    builder.Prompts.text(session, "Okay, what do we need to prepare it?");
-  },
-  function (session, result) {
-    session.userData.preparation = result.response;
-    builder.Prompts.time(session, "When should we do that?");
-  },
-  function (session, result) {
-    session.userData.schedule = builder.EntityRecognizer.resolveTime([result.response]);
-    console.log(session.userData.schedule);
-    session.send("Great - sounds like a plan!");
-    session.endDialog();
+    builder.Prompts.text(session,
+                         "Hi! Let's plan dinners for the week!"
+                        );
   }
 ]);
 
